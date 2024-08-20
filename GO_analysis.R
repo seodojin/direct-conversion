de2d3d6e3aefaa7a8ac54b532051c98093981645
seodojin <82428437+seodojin@users.noreply.github.com>
2024-08-21 02:01:53 +0900

    Update GO_analysis.R

--- GO_analysis.R
+++ GO_analysis.R
@@ -10,6 +10,8 @@ library(org.Hs.eg.db)
 library(dorothea)
 library(viper)
 library(corrplot)
+library(igraph)
+library(ggraph)
 
 # 데이터 로드 및 전처리
 loaded_results <- readRDS("20240808_fitGAM_results_full.rds")
@@ -38,29 +40,6 @@ compare_lineages <- function(lineage1, lineage2) {
 significant_genes_1_vs_2 <- compare_lineages("Lineage1", "Lineage2")
 significant_genes_2_vs_3 <- compare_lineages("Lineage2", "Lineage3")
 
-# GO 분석
-perform_go_analysis <- function(gene_list) {
-  ego <- enrichGO(gene = gene_list,
-                  OrgDb = org.Hs.eg.db,
-                  keyType = "SYMBOL",
-                  ont = "BP",
-                  pAdjustMethod = "BH",
-                  pvalueCutoff = 0.05,
-                  qvalueCutoff = 0.05)
-  return(ego)
-}
-
-ego_1_vs_2 <- perform_go_analysis(significant_genes_1_vs_2)
-ego_2_vs_3 <- perform_go_analysis(significant_genes_2_vs_3)
-
-# Barplot for ego_1_vs_2
-barplot(ego_1_vs_2, showCategory=20)  # 상위 20개 용어 보여주기
-
-# Barplot for ego_2_vs_3
-barplot(ego_2_vs_3, showCategory=20)  # 상위 20개 용어 보여주기
-
-
-
 # VIPER 분석 함수
 perform_viper_analysis <- function(gene_list) {
   common_genes <- intersect(rownames(sce_fitted), gene_list)
@@ -79,17 +58,12 @@ perform_viper_analysis <- function(gene_list) {
   return(list(tf_activities = tf_activities, regulon = regulon))
 }
 
-# Lineage 1 vs 2 VIPER 분석
+# VIPER 분석 실행
 viper_results_1_vs_2 <- perform_viper_analysis(significant_genes_1_vs_2)
-tf_activities_1_vs_2 <- viper_results_1_vs_2$tf_activities
-regulon_1_vs_2 <- viper_results_1_vs_2$regulon
-
-# Lineage 2 vs 3 VIPER 분석
 viper_results_2_vs_3 <- perform_viper_analysis(significant_genes_2_vs_3)
-tf_activities_2_vs_3 <- viper_results_2_vs_3$tf_activities
-regulon_2_vs_3 <- viper_results_2_vs_3$regulon
 
-# 결과 시각화 함수 수정
+# VIPER Network Visualization
+# 결과 시각화 함수
 visualize_tf_activities <- function(tf_activities, title, sample_size = 1000) {
   set.seed(123)
   sample_cols <- sample(1:ncol(tf_activities), min(sample_size, ncol(tf_activities)))
@@ -114,30 +88,24 @@ visualize_tf_activities <- function(tf_activities, title, sample_size = 1000) {
   
   cor_matrix <- cor(t(tf_activities_sampled[top_20_tfs,]))
   
-  # 여백을 늘려 제목이 잘리지 않게 수정
   png(paste0("correlation_top20_tf_activities_", gsub(" ", "_", title), ".png"), width = 6, height = 6, units = "in", res = 600)
-  par(mar = c(4, 4, 6, 2))  # 상단 여백을 6으로 설정하여 제목 공간을 늘림
+  par(mar = c(4, 4, 6, 2))
   corrplot(cor_matrix, method = "color", type = "upper",
            order = "hclust", tl.col = "black", tl.srt = 45)
-  
-  # mtext로 제목 추가
   mtext(paste("Correlation of Top 20 TF Activities -", title), side = 3, line = 3, cex = 1.2)
-  
   dev.off()
   
-  gc()  # 메모리 정리
+  gc()
   return(top_20_tfs)
 }
 
-# Lineage 1 vs 2 결과 시각화
-top_20_tfs_1_vs_2 <- visualize_tf_activities(tf_activities_1_vs_2, "Lineage 1 vs 2")
+# 시각화 실행
+top_20_tfs_1_vs_2 <- visualize_tf_activities(viper_results_1_vs_2$tf_activities, "Lineage 1 vs 2")
+top_20_tfs_2_vs_3 <- visualize_tf_activities(viper_results_2_vs_3$tf_activities, "Lineage 2 vs 3")
 
-# Lineage 2 vs 3 결과 시각화
-top_20_tfs_2_vs_3 <- visualize_tf_activities(tf_activities_2_vs_3, "Lineage 2 vs 3")
-
-# 상위 활성 전사 인자들의 타겟 유전자 분석
+# 상위 전사 인자 타겟 분석
 analyze_top_tf_targets <- function(top_tfs, regulon, title) {
-  for (tf in top_tfs[1:5]) {  # 상위 5개 전사 인자에 대해 분석
+  for (tf in top_tfs[1:5]) {
     targets <- names(regulon[[tf]]$tfmode)
     cat("Top targets for", tf, "in", title, ":\n")
     print(head(targets, 10))
@@ -145,9 +113,145 @@ analyze_top_tf_targets <- function(top_tfs, regulon, title) {
   }
 }
 
-# Lineage 1 vs 2의 상위 전사 인자 타겟 분석
-analyze_top_tf_targets(top_20_tfs_1_vs_2, regulon_1_vs_2, "Lineage 1 vs 2")
+# 타겟 분석 실행
+analyze_top_tf_targets(top_20_tfs_1_vs_2, viper_results_1_vs_2$regulon, "Lineage 1 vs 2")
+analyze_top_tf_targets(top_20_tfs_2_vs_3, viper_results_2_vs_3$regulon, "Lineage 2 vs 3")
 
-# Lineage 2 vs 3의 상위 전사 인자 타겟 분석
-analyze_top_tf_targets(top_20_tfs_2_vs_3, regulon_2_vs_3, "Lineage 2 vs 3")
+# 네트워크 시각화 함수
+visualize_tf_target_network <- function(top_tfs, regulon, title, top_n = 3, max_targets_per_tf = 10, file_name) {
+  edges <- data.frame()
+  
+  for (tf in top_tfs[1:top_n]) {
+    targets <- names(regulon[[tf]]$tfmode)
+    targets <- head(targets, max_targets_per_tf)
+    df <- data.frame(from = rep(tf, length(targets)), to = targets)
+    edges <- rbind(edges, df)
+  }
+  
+  graph <- graph_from_data_frame(edges)
+  
+  plot <- ggraph(graph, layout = 'kk') +
+    geom_edge_link(aes(edge_alpha = 0.8), show.legend = FALSE) +
+    geom_node_point(color = "blue", size = 5) +
+    geom_node_text(aes(label = name), vjust = 1, hjust = 1) +
+    ggtitle(paste("TF-Target Network -", title)) +
+    theme_void()
+  
+  print(plot)
+  ggsave(filename = file_name, plot = plot, width = 10, height = 8, dpi = 300)
+}
 
+# 네트워크 시각화 저장
+visualize_tf_target_network(top_20_tfs_1_vs_2, viper_results_1_vs_2$regulon, "Lineage 1 vs 2", top_n = 5, max_targets_per_tf = 5, file_name = "lineage_1_vs_2_network.png")
+visualize_tf_target_network(top_20_tfs_2_vs_3, viper_results_2_vs_3$regulon, "Lineage 2 vs 3", top_n = 5, max_targets_per_tf = 5, file_name = "lineage_2_vs_3_network.png")
+
+
+
+
+# TRRUST 네트워크 시각화 함수
+visualize_trrust_network <- function(tf_genes, title, file_name) {
+  graph <- graph_from_data_frame(tf_genes, directed = FALSE)
+  
+  plot <- ggraph(graph, layout = "kk") +
+    geom_edge_link(aes(edge_alpha = 0.8), color = "grey") +
+    geom_node_point(size = 5, color = "skyblue") +
+    geom_node_text(aes(label = name), vjust = 1.5, hjust = 0.5, size = 4) +
+    theme_void() +
+    labs(title = title) +
+    guides(edge_alpha = "none")
+  
+  print(plot)
+  ggsave(filename = file_name, plot = plot, width = 8, height = 6, dpi = 600)
+}
+
+# Lineage 1 vs 2 데이터 및 시각화
+tf_genes12 <- data.frame(
+  Key_TF = c("RB1", "RB1", "RB1", "MYB", "MYB", "MYB", "RBL1", "RBL1", "NF1", "NF1", "ETS2", "ETS2", "TP53", "TP53", "TP53", "NR3C1", "NR3C1", "HNF4A", "HNF4A", "WT1", "WT1", "AR", "AR", "E2F1", "E2F1", "SP1", "SP1", "SP1", "RELA", "RELA", "NFKB1", "NFKB1"),
+  Gene = c("MYC", "ATF2", "TFDP1", "KLF1", "MYC", "SP3", "TFDP1", "MYC", "SP3", "MYC", "MYC", "ERG", "MYC", "SMAD3", "E2F7", "SRF", "ATF2", "TBP", "MYC", "SMAD3", "MYC", "MYC", "TBP", "ETV4", "MYC", "ATF2", "SP3", "MYC", "MYC", "TBP", "TBP", "MYC")
+)
+
+visualize_trrust_network(tf_genes12, "Network of Key TFs and Overlapped Genes - Lineage 1 vs 2", "20240820_lineage1vs2_trrust_network.png")
+
+# Lineage 2 vs 3 데이터 및 시각화
+tf_genes23 <- data.frame(
+  Key_TF = c("RB1", "RB1", "RB1", "MYB", "MYB", "MYB", "CEBPE", "CEBPE", "MYBL2", "MYBL2", "NF1", "NF1", "HDAC2", "HDAC2", "ETS2", "ETS2", "TP53", "TP53", "TP53", "NR3C1", "NR3C1", "HNF4A", "HNF4A", "CEBPA", "CEBPA", "FOS", "FOS", "GATA1", "GATA1", "WT1", "WT1", "ESR1", "ESR1", "AR", "AR", "JUN", "JUN", "SP1", "SP1", "SP1", "RELA", "RELA", "NFKB1", "NFKB1"),
+  Gene = c("MYC", "ATF2", "SP1", "KLF1", "MYC", "SP3", "SPI1", "MYC", "SP1", "MYC", "SP3", "MYC", "MYC", "SP1", "MYC", "ERG", "MYC", "SMAD3", "E2F7", "SRF", "ATF2", "TBP", "MYC", "SPI1", "MYC", "SPI1", "MYC", "SPI1", "KLF1", "SMAD3", "MYC", "MYC", "SP1", "MYC", "TBP", "SPI1", "MYC", "ATF2", "SP3", "MYC", "MYC", "TBP", "TBP", "MYC")
+)
+
+visualize_trrust_network(tf_genes23, "Network of Key TFs and Overlapped Genes - Lineage 2 vs 3", "20240820_lineage2vs3_trrust_network.png")
+
+
+
+##### trrust GO
+# 유전자 목록 준비 (예시로 tf_genes12 사용)
+genes12 <- unique(c(tf_genes12$Key_TF, tf_genes12$Gene))
+
+# 유전자 이름을 Entrez ID로 변환
+entrez_ids12 <- mapIds(org.Hs.eg.db, keys = genes12, keytype = "SYMBOL", column = "ENTREZID")
+entrez_ids12 <- entrez_ids12[!is.na(entrez_ids12)]
+
+# GO 분석 수행
+go_results12 <- enrichGO(gene = entrez_ids12,
+                       OrgDb = org.Hs.eg.db,
+                       ont = "BP",  # 모든 GO 범주 (BP, CC, MF)
+                       pAdjustMethod = "BH",
+                       pvalueCutoff = 0.05,
+                       qvalueCutoff = 0.05)
+
+# 결과 확인
+head(go_results12)
+
+# 결과 시각화
+# 상위 10개 GO 용어의 막대 그래프
+barplot(go_results12, showCategory = 15)
+ggsave("TRRUST_lineage1vs2_GO_barplot.png", width = 12, height = 12, dpi=600)
+
+# GO 네트워크 그래프
+# 용어 간 유사성 계산
+library(enrichplot)
+go_results_sim12 <- pairwise_termsim(go_results12)
+
+# GO 네트워크 그래프
+go_network12 <- enrichplot::emapplot(go_results_sim12, showCategory = 20)
+ggsave("TRRUST_lineage1vs2_GO_network.png", plot = go_network12, width = 15, height = 12.5)
+
+
+# 결과를 CSV 파일로 저장
+write.csv(as.data.frame(go_results12), "TRRUST_lineage1vs2_GO_analysis_results.csv", row.names = FALSE)
+
+
+
+# 유전자 목록 준비 (예시로 tf_genes12 사용)
+genes23 <- unique(c(tf_genes23$Key_TF, tf_genes23$Gene))
+
+# 유전자 이름을 Entrez ID로 변환
+entrez_ids23 <- mapIds(org.Hs.eg.db, keys = genes23, keytype = "SYMBOL", column = "ENTREZID")
+entrez_ids23 <- entrez_ids23[!is.na(entrez_ids23)]
+
+# GO 분석 수행
+go_results23 <- enrichGO(gene = entrez_ids23,
+                       OrgDb = org.Hs.eg.db,
+                       ont = "BP",  # 모든 GO 범주 (BP, CC, MF)
+                       pAdjustMethod = "BH",
+                       pvalueCutoff = 0.05,
+                       qvalueCutoff = 0.05)
+
+# 결과 확인
+head(go_results23)
+
+# 결과 시각화
+# 상위 20개 GO 용어의 막대 그래프
+barplot(go_results23, showCategory = 20)
+ggsave("TRRUST_lineage2vs3_GO_barplot.png", width = 12, height = 12, dpi=600)
+
+# GO 네트워크 그래프
+
+go_results_sim23 <- pairwise_termsim(go_results23)
+
+# GO 네트워크 그래프
+go_network23 <- enrichplot::emapplot(go_results_sim23, showCategory = 20)
+ggsave("TRRUST_lineage2vs3_GO_network.png", plot = go_network23, width = 15, height = 12.5)
+
+
+# 결과를 CSV 파일로 저장
+write.csv(as.data.frame(go_results23), "TRRUST_lineage2vs3_GO_analysis_results.csv", row.names = FALSE)
