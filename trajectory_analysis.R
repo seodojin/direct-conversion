09909a12e6935d9327543b5d05666993b20fcb57
seodojin <82428437+seodojin@users.noreply.github.com>
2024-09-20 13:25:06 +0900

    Update trajectory_analysis.R

--- trajectory_analysis.R
+++ trajectory_analysis.R
@@ -8,20 +8,38 @@ library(clusterExperiment)
 library(scater)
 library(dplyr)
 library(mice)
+library(tidyr)
+library(ggplot2)
+library(ggridges)
 
 # Seurat 객체 불러오기 및 SingleCellExperiment 변환
 plus <- readRDS("seurat_object2")
 
-# Seurat 객체에서 RNA 카운트 및 정규화 데이터를 추출하여 두 조건을 결합
-counts <- cbind(plus[["RNA"]]$counts.shCtrl, plus[["RNA"]]$counts.shPTBP1)
-data <- cbind(plus[["RNA"]]$data.shCtrl, plus[["RNA"]]$data.shPTBP1)
+# Seurat 객체에서 meta.data에서 shPTBP1에 해당하는 세포만 선택
+shPTBP1_cells <- rownames(plus@meta.data[plus@meta.data$orig.ident == "shPTBP1", ])
 
-# Seurat 객체에서 추출한 카운트, 정규화 데이터, 메타데이터 및 차원 축소 결과를 사용하여 SCE 객체 생성
-sce <- SingleCellExperiment(
-  assays = list(counts = counts, logcounts = data),
-  colData = plus@meta.data,
-  reducedDims = list(PCA = Embeddings(plus, "pca"), UMAP = Embeddings(plus, "umap"))
+# 선택된 shPTBP1 세포들에 대한 RNA 카운트 및 정규화 데이터 추출
+counts_shPTBP1 <- plus[["RNA"]]$counts.shPTBP1[, shPTBP1_cells]
+data_shPTBP1 <- plus[["RNA"]]$data.shPTBP1[, shPTBP1_cells]
+
+# 필터링된 메타데이터
+meta_data_shPTBP1 <- plus@meta.data[shPTBP1_cells, ]
+
+# 필터링된 PCA 및 UMAP 차원 축소 결과
+pca_shPTBP1 <- Embeddings(plus, "pca")[shPTBP1_cells, ]
+umap_shPTBP1 <- Embeddings(plus, "umap")[shPTBP1_cells, ]
+
+# 필터링된 데이터를 사용하여 SingleCellExperiment 객체 생성
+sce_shPTBP1 <- SingleCellExperiment(
+  assays = list(counts = counts_shPTBP1, logcounts = data_shPTBP1),
+  colData = meta_data_shPTBP1,
+  reducedDims = list(PCA = pca_shPTBP1, UMAP = umap_shPTBP1)
 )
+
+# 필터링된 SCE 객체 확인
+sce_shPTBP1
+
+sce <- sce_shPTBP1
 
 # 신경세포(GABAergic 및 Glutamatergic neurons)를 "Neurons"로 통합하고 나머지는 기존 라벨 유지
 sce$updated_customclassif <- sapply(sce$customclassif, function(label) {
@@ -32,31 +50,21 @@ sce$updated_customclassif <- sapply(sce$customclassif, function(label) {
   }
 })
 
-# Fibroblasts 세포 추출 및 무작위로 100개 샘플링
-# (분석의 초점을 신경세포 및 다른 세포들의 궤적과 발달 경로에 맞추기 위한 것)
-fibroblast_cells <- which(sce$updated_customclassif == "Fibroblasts")
-non_fibroblast_cells <- which(sce$updated_customclassif != "Fibroblasts")
-set.seed(123)  # 샘플링의 재현성을 위해 시드 설정
-sampled_fibroblasts <- sample(fibroblast_cells, size = 100)
-
-# 샘플링된 섬유아세포와 나머지 세포를 병합하여 최종 데이터셋 생성
-sce_reduced <- sce[, c(sampled_fibroblasts, non_fibroblast_cells)]
-
 # Slingshot을 사용하여 세포 궤적 분석 수행, 섬유아세포를 시작점으로 설정
-sds_reduced <- slingshot(sce_reduced, clusterLabels = 'updated_customclassif', reducedDim = 'UMAP',
+sds <- slingshot(sce, clusterLabels = 'updated_customclassif', reducedDim = 'UMAP',
                          start.clus = "Fibroblasts", end.clus = c("Myofibroblasts", "Immature neurons", "Neurons"))
 
 # 유사시간(pseudotime)과 각 리니지에 대한 세포 가중치(cell weights)를 추출
-pseudotime_reduced <- as.data.frame(slingPseudotime(sds_reduced))
-cell_weights_reduced <- slingCurveWeights(sds_reduced)
+pseudotime <- as.data.frame(slingPseudotime(sds))
+cell_weights <- slingCurveWeights(sds)
 
 # Fibroblasts는 무작위로 리니지를 할당하고, 나머지 세포는 가중치가 가장 높은 리니지에 할당
-pseudotime_reduced$MainLineage <- sapply(seq_len(nrow(pseudotime_reduced)), function(i) {
-  if (sce_reduced$updated_customclassif[i] == "Fibroblasts") {
+pseudotime$MainLineage <- sapply(seq_len(nrow(pseudotime)), function(i) {
+  if (sce$updated_customclassif[i] == "Fibroblasts") {
     return(sample(c("Lineage1", "Lineage2", "Lineage3"), 1))  # Fibroblasts는 무작위로 리니지 할당
   } else {
-    lineages <- which(!is.na(pseudotime_reduced[i, ]))
-    cell_weights_row <- cell_weights_reduced[i, lineages]
+    lineages <- which(!is.na(pseudotime[i, ]))
+    cell_weights_row <- cell_weights[i, lineages]
     main_lineage <- lineages[which.max(cell_weights_row)]
     return(paste0("Lineage", main_lineage))
   }
@@ -72,15 +80,15 @@ normalize_pseudotime <- function(pseudotime) {
 }
 
 # 각 리니지의 유사시간을 0과 1 사이로 정규화
-filtered_pseudotime <- pseudotime_reduced[, c("Lineage1", "Lineage2", "Lineage3")]
+filtered_pseudotime <- pseudotime[, c("Lineage1", "Lineage2", "Lineage3")]
 normalized_pseudotime <- normalize_pseudotime(filtered_pseudotime)
 
 # 정규화된 유사시간에 해당하는 세포 이름을 추출
 valid_cells_pseudotime <- rownames(normalized_pseudotime)
 
 # Slingshot 결과와 일치하는 세포만 필터링하여 새로운 SCE 객체 생성
-sce_filtered_subset <- sce_reduced[, valid_cells_pseudotime]
-filtered_weights <- cell_weights_reduced[valid_cells_pseudotime, , drop = FALSE]
+sce_filtered_subset <- sce[, valid_cells_pseudotime]
+filtered_weights <- cell_weights[valid_cells_pseudotime, , drop = FALSE]
 filtered_counts <- assay(sce_filtered_subset, "counts")
 
 # 필터링 후 각 데이터셋의 차원을 확인
@@ -158,10 +166,6 @@ sds_new <- slingshot(sce_filtered, clusterLabels = 'updated_customclassif', redu
 new_pseudotime <- slingPseudotime(sds_new)
 new_weights <- slingCurveWeights(sds_new)
 
-# NA 값 확인
-print("새로운 pseudotime의 NA 개수:")
-print(colSums(is.na(new_pseudotime)))
-
 print("새로운 weights의 NA 개수:")
 print(colSums(is.na(new_weights)))
 
@@ -196,23 +200,7 @@ print(dim(final_pseudotime))
 print(dim(final_weights))
 print(dim(final_counts))
 
-######################################################
-
-# 리니지별 최소 세포 수를 기반으로 각 리니지에서 균형 잡힌 샘플링 진행
-min_cells <- min(table(lineage_assignment[valid_cells]))
-set.seed(42)  # 샘플링의 재현성을 위해 시드 설정
-balanced_cells <- unlist(lapply(unique(lineage_assignment[valid_cells]), function(i) {
-  sample(which(lineage_assignment[valid_cells] == i), min_cells)
-}))
-
-# 균형 잡힌 데이터셋 생성
-balanced_pseudotime <- final_pseudotime[balanced_cells, ]
-balanced_weights <- final_weights[balanced_cells, ]
-balanced_counts <- final_counts[, balanced_cells]
-
-# 균형 잡힌 데이터셋의 리니지별 세포 수 출력
-print("균형 잡힌 데이터셋의 리니지별 세포 수:")
-print(table(lineage_assignment[valid_cells][balanced_cells]))
+###########################################################
 
 # 0과 1 사이로 유사시간을 정규화
 normalize_pseudotime <- function(pseudotime) {
@@ -226,7 +214,7 @@ normalize_pseudotime <- function(pseudotime) {
 }
 
 # 유사시간 정규화 적용
-normalized_pseudotime <- normalize_pseudotime(balanced_pseudotime)
+normalized_pseudotime <- normalize_pseudotime(final_pseudotime)  # balanced_pseudotime 대신 final_pseudotime 사용
 
 # 정규화된 유사시간 확인
 head(normalized_pseudotime)
@@ -240,40 +228,73 @@ imputed_pseudotime <- complete(imputed_data, action = 1)
 # 대체된 pseudotime 확인
 head(imputed_pseudotime)
 
+#######################################################
+
+# 유효한 세포의 인덱스에 해당하는 클러스터 정보 추출
+valid_clusters <- sce_filtered$updated_customclassif[valid_cells]
+
+# 각 세포에서 가장 높은 가중치를 가진 리니지에 해당하는 Pseudotime만 남김
+pseudotime_long <- data.frame(
+  Pseudotime = imputed_pseudotime[cbind(1:nrow(imputed_pseudotime), lineage_assignment)],  # 가장 높은 가중치의 유사시간만 선택
+  Lineage = factor(lineage_assignment),  # 각 세포가 할당된 리니지
+  CellWeights = final_weights[cbind(1:nrow(final_weights), lineage_assignment)],  # 각 세포의 리니지별 가중치
+  Cluster = valid_clusters  # 각 세포가 속한 클러스터 정보
+)
+
+
+# 밀도 기반 플롯 생성 (4x1 배치)
+
+cluster_colors <- c("Immature neurons" = "#00bef3", 
+                    "Myofibroblasts" = "#ff8c8c", 
+                    "Fibroblasts" = "#19c3a3", 
+                    "Neurons" = "#d4a600")
+
+density_plot <- ggplot(pseudotime_long, aes(x = Pseudotime, fill = Cluster, weight = CellWeights)) +
+  geom_density(alpha = 0.7) +
+  scale_fill_manual(values = cluster_colors) +  
+  facet_grid(Cluster ~ ., scales = "free_y") +  # 각 클러스터를 행으로 설정하여 4x1 배치로 변경
+  labs(
+    title = "Pseudotime Progression Along Differentiation Trajectories",
+    x = "Pseudotime",
+    y = "Density"
+  ) +
+  theme_minimal() +
+  theme(legend.position = "none")
+
+# 플롯 확인
+print(density_plot)
+
+# 그림을 600dpi로 저장
+ggsave("20240920_pseudotime_vs_cluster_weights.png", plot = density_plot, dpi = 600, width = 8, height = 12)
+
+##########################################################
+
 # 대체된 pseudotime을 사용하여 fitGAM 실행
 sce_fitted <- fitGAM(
-  counts = balanced_counts,
+  counts = final_counts,  # balanced_counts 대신 final_counts 사용
   pseudotime = imputed_pseudotime,
-  cellWeights = balanced_weights,
+  cellWeights = final_weights,  # balanced_weights 대신 final_weights 사용
   nknots = 5,    # 궤적에 대한 매끄러움을 조절하는 매개변수
   verbose = TRUE,
   parallel = FALSE
 )
 
-
 ######################################################
 # 각 세포를 가장 높은 가중치를 가진 리니지에 할당
-lineage_assignment <- apply(balanced_weights, 1, which.max)
-
+lineage_assignment <- apply(final_weights, 1, which.max)  
 # 리니지별 세포 수 계산
 lineage_counts <- table(lineage_assignment)
-
 # 결과 출력
 print("리니지별 세포 수:")
 print(lineage_counts)
 
-
 # fitGAM 결과 저장
 saveRDS(sce_fitted, file = "20240910_post_fitGAM.rds")
-
 # Slingshot 객체 저장
 saveRDS(sds_filtered, file = "slingshot_obj.rds")
-
 # pseudotime 저장
 saveRDS(imputed_pseudotime, file = "pseudotime.rds")
-
 # cell weights 저장
-saveRDS(balanced_weights, file = "cell_weights.rds")
-
-# balanced_counts 객체를 RDS 파일로 저장
-saveRDS(balanced_counts, file = "balanced_counts.rds")
+saveRDS(final_weights, file = "cell_weights.rds")  
+# counts 객체 저장
+saveRDS(final_counts, file = "final_counts.rds")  
